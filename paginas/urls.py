1b90d6fae7b0ad888dd1e23b3a6a179da2eddab5
jnevesthe <nevesjosemar9@gmail.com>
2025-07-18 14:42:00 +0100

    Update urls.py

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import Index, Sobre
+from django.http import FileResponse
+import os
 
-urlpatterns=[
-    
+urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("sobre/", Sobre.as_view(), name="sobre"),
+    
+    # Rota para ads.txt
+    path("ads.txt", lambda request: FileResponse(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "ads.txt"), "rb"))),
 ]
